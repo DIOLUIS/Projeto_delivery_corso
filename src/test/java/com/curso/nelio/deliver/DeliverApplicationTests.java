87e9f9d0c5320414b2aba580296714f5ac64efd6
dioluis <diogoparazita@gmail.com>
2021-05-28 10:16:33 -0300

    Iniciando o projeto

---
+++ src/test/java/com/curso/nelio/deliver/DeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.nelio.deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
